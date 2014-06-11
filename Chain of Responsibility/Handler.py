1bc3c4df392918d9799c5abf4566ea95e13af06a
pdr <Pedro>
2014-06-10 21:04:40 -0300

    terminado

--- Chain of Responsibility/Handler.py
+++ Chain of Responsibility/Handler.py
@@ -1,8 +1,48 @@
 class Handler:
 	sucessor = None
 
-	def setSucessor(self, sucessor):
-		self.sucessor = sucessor
+	def setSucessor(self, suc):
+		self.sucessor = suc
+
+	def handleRequest(self, request): pass
+
+
+
+class ConcreteHandler1(Handler):
+	def handleRequest(self, request):
+		if (request >= 0 and request < 10):
+			print("{0} handled request: {1}".format(__class__.__name__, request))
+		if (self.sucessor != None):
+			self.sucessor.handleRequest(request)
+
+class ConcreteHandler2(Handler):
+	def handleRequest(self, request):
+		if (request >= 10 and request < 20):
+			print("{0} handled request: {1}".format(__class__.__name__, request))
+		if (self.sucessor != None):
+			self.sucessor.handleRequest(request)
+
+class ConcreteHandler3(Handler):
+	def handleRequest(self, request):
+		if (request >= 20 and request < 30):
+			print("{0} handled request: {1}".format(__class__.__name__, request))
+		if (self.sucessor != None):
+			self.sucessor.handleRequest(request)
+			
+
+
+
+a = ConcreteHandler1()
+b = ConcreteHandler2()
+c = ConcreteHandler3()
+
+a.setSucessor(b)
+b.setSucessor(c)
+
+requests = list(range(0, 40))
+
+for e in requests:
+	a.handleRequest(e)
 
 
 
